f04c9e7f0d2aa3ee9d2f3992ef25207bf1d8f517
AprilAlfonso <AprilAlfonso@users.noreply.github.com>
2020-07-31 00:11:51 +0800

    Edited collapsible sections in published works

--- script.js
+++ script.js
@@ -44,19 +44,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
-// Collapsible Content
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
-}
